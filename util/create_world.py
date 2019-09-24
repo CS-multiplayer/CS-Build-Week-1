0952a80bb874263fc7233b54f605da9de511f0b1
charles-rogers <c.a.r0g3r5@gmail.com>
2019-09-24 18:20:10 -0500

    yeap

--- util/create_world.py
+++ util/create_world.py
@@ -1,13 +1,13 @@
 import random 
 from django.contrib.auth.models import User
-from adventure.models import Room
+from adventure.models import Room, Player, World
 from adventure.models import Player
 from adventure.models import World
 
 Room.objects.all().delete()
 
-r = Room(title= 'title', description= '')
-r.testFunc()
+# r = Room(title= 'title', description= '')
+# r.testFunc()
 
 w = World()
 num_rooms = 115
@@ -18,5 +18,5 @@ w.generate_rooms(width, height, num_rooms)
 
 players = Player.objects.all()
 for p in players:
-    p.currentRoom = w.grid[0].id
+    p.current_rooms = w.grid[0][0].id
     p.save()
